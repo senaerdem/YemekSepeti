66d99a337bc3518309d2506b2fbb444787f9aae6
senaerdem <snnrrdm@gmail.com>
2023-11-13 15:12:48 +0300

    constant

---
+++ App/Constant.swift
@@ -0,0 +1,36 @@
+//
+//  Constant.swift
+//  App
+//
+//  Created by Sena Nur Erdem  on 04.11.2023.
+//
+import Foundation
+
+public let userDefaults = UserDefaults.standard
+
+public let kID = "id"
+public let kNAME = "name"
+public let kPRICE = "price"
+public let kDESCRIPTION = "description"
+public let kCATEGORY = "category"
+public let kIMAGENAME = "imageName"
+
+
+//Order
+public let kITEMIDS = "itemIds"
+public let kOWNERID = "ownerId"
+public let kCUSTOMERID = "customerId"
+public let kAMOUNT = "amount"
+public let kCUSTOMERNAME = "customerName"
+public let kISCOMPLETED = "isCompleted"
+
+
+//FUser
+public let kEMAIL = "email"
+public let kFIRSTNAME = "firstname"
+public let kLASTNAME = "lastname"
+public let kFULLNAME = "fullname"
+public let kCURRENTUSER = "currentUser"
+public let kFULLADDRESS = "fullAddress"
+public let kPHONENUMBER = "phoneNumber"
+public let kONBOARD = "onBoard"
